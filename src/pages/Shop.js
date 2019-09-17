04e164cb1df11ee6ef74dbc7b8001d3f479efe67
dani690e <dani690e@videndjurs.net>
2019-09-17 10:21:29 +0200

    Create Shop.js

---
+++ src/pages/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import header from '../header/navBar'
+
+const Shop = () => {
+    return (
+        <div className="Shop">
+            <section className="shopNav"></section>
+            <section className="products"></section>
+        </div>
+    );
+}
+ 
+export default Shop;
