84d8d4fd9a1c38b2b0d574e2f95b4a93ed322f32
dani690e <dani690e@videndjurs.net>
2019-09-17 11:55:13 +0200

    Update navBar.js

--- src/header/navBar.js
+++ src/header/navBar.js
@@ -5,21 +5,21 @@ import { Link, NavLink, withRouter } from 'react-router-dom'
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#"><img src="../src/images/runinc_logo.svg" alt="" /></a>
+            <NavLink className="navbar-brand font-weight-bold text-danger" href="#"><img src="../src/images/runinc_logo.svg" alt="" />RUN<span className="text-dark">INC</span></NavLink>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <ul className="navbar-nav">
-                    <li className="nav-item nav-link active"><NavLink to="/">Home</NavLink></li>
-                    <li className="nav-item nav-link"><NavLink to="/event">Event</NavLink></li>
-                    <li className="nav-item nav-link"><NavLink to="/shop">Shop</NavLink></li>
-                    <li className="nav-item nav-link"><NavLink to="/contact">Contact</NavLink></li>
+                    <li className="nav-item nav-link active"><NavLink className="text-dark" to="/">Home</NavLink></li>
+                    <li className="nav-item nav-link"><NavLink className="text-dark" to="/event">Event</NavLink></li>
+                    <li className="nav-item nav-link"><NavLink className="text-dark" to="/shop">Shop</NavLink></li>
+                    <li className="nav-item nav-link"><NavLink className="text-dark" to="/contact">Contact</NavLink></li>
                 </ul>
             </div>
             <form className="form-inline">
-                <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                <input className="bg-warning text-white border border-dark" type="search" aria-label="Search"/>
+                <button className="btn btn-outline-light bg-dark text-white" type="submit">Search</button>
             </form>
         </nav>
             )
