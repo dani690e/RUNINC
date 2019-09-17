f43e22c7036be1f20edb97e42688e3720715cb66
dani690e <dani690e@videndjurs.net>
2019-09-17 20:41:37 +0200

    Update Shop.js

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Shop = () => {
+    const [brands] = useState([
+        { Brand: "Nike"},
+        { Brand: "Adidas"},
+        { Brand: "Puma"},
+        { Brand: "Asics"},
+        { Brand: "Hummel"}
+    ]);
+    const BrandList = brands.map(brand => {
+        return (
+            <div className="brandName" key={Math.random()}>
+                    <p className="text-decoration-none">{brand.Brand}</p>
+            </div>
+        )
+    })
     return (
         <div className="Shop">
-            <section className="shopNav"></section>
+            <section className="shopNav">
+                <div className="shopInfo">
+                    <h3>SHOP</h3>
+                    <h4>RUNINC SHOP</h4>
+                    <p>Gratis fragt ved over 500.-</p>
+                </div>
+                {BrandList}
+            </section>
             <section className="products"></section>
         </div>
     );
