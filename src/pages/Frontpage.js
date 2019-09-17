657317f5276b528d99562f841c83e8966a178cc8
dani690e <dani690e@videndjurs.net>
2019-09-17 10:21:35 +0200

    Create Frontpage.js

---
+++ src/pages/Frontpage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Frontpage = () => {
+    return (
+        <div className="Frontpage">
+            <h2>We Love <span>Running</span></h2>
+        </div>
+    );
+}
+ 
+export default Frontpage;
