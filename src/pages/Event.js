80c32fd27f54c50fda3dbc5ecaa8079ae6da4a01
dani690e <dani690e@videndjurs.net>
2019-09-17 10:21:37 +0200

    Create Event.js

---
+++ src/pages/Event.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import header from '../header/navBar'
+
+const Event = () => {
+    return (
+        <div className="Event">
+            <section></section>
+            <aside></aside>
+        </div>
+    );
+}
+ 
+export default Event;
