95b083aac6ea3af44647f3724c9ba63e0d35ced9
dani690e <dani690e@videndjurs.net>
2019-09-17 20:41:39 +0200

    Update Frontpage.js

--- src/pages/Frontpage.js
+++ src/pages/Frontpage.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import Event from './Event'
+import Shop from './Shop'
+import Contact from './Contact'
 
 const Frontpage = () => {
     return (
-        <div className="Frontpage">
-            <h2>We Love <span>Running</span></h2>
+        <div className="pages">
+            <div className="Frontpage">
+                <h2 className="text-white">We Love <span className="text-warning">Running</span></h2>
+            </div>
+            <Event/>
+            <Shop/>
+            <Contact/>
         </div>
     );
 }
- 
+
 export default Frontpage;
