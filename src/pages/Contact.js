8640a37307cd8724591ac56a761ed649c0574cf8
dani690e <dani690e@videndjurs.net>
2019-09-18 12:15:47 +0200

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,22 +1,32 @@
 import React from 'react'
+import map from '../images/map.JPG'
 
 const contact = () => {
     return (
-        <div className="Contact container d-flex">
-            <section className="contactInfo row col-6">
-                <h2>Contact</h2>
+        <div className="Contact d-flex">
+            <section className="contactInfo col-12 col-md-6">
+                <h2 className="bg-yellow border border-dark">Contact</h2>
+                <img src={map} alt="" className="img-fluid"/>
+                <div className="information text-center text-white">
+                    <h3 className="text-center">Running</h3>
+                    <p>Kannikegade 12</p>
+                    <p>8000 Aarhus C</p>
+                    <p>Telefon: 26 13 26 96</p>
+                    <p><u>info@runinc.dk</u></p>
+                </div>
             </section>
-            <section className="contact row col-6">
-                <h3>Send en besked</h3>
+            <section className="contact col-12 col-md-6">
+                <h3 className="text-center">Send en besked</h3>
                 <form className="d-flex flex-column">
-                    <label htmlFor="name">Navn:</label>
+                    <label htmlFor="name">NAVN:</label>
                     <input type="text" id="name"/>
-                    <label htmlFor="email">Email:</label>
+                    <label htmlFor="email">EMAIL:</label>
                     <input type="email" id="email"/>
-                    <label htmlFor="tlf">Telefonnr.:</label>
+                    <label htmlFor="tlf">TELEFONNR.:</label>
                     <input type="number" id="tlf"/>
-                    <label htmlFor="besked">Besked:</label>
-                    <textarea name="besked" id="besked" cols="30" rows="10"></textarea>
+                    <label htmlFor="besked">BESKED:</label>
+                    <textarea name="besked" id="besked" cols="30" rows="5"></textarea>
+                    <button>Send besked</button>
                 </form>
             </section>
         </div>
